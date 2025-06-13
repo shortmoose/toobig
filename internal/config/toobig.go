f74ca8d91509565bdbd97e62ddecf2847dd7e93b
Nathan Bullock <nathanbullock@gmail.com>
2025-06-13 01:42:06 -0700

    x

--- internal/config/toobig.go
+++ internal/config/toobig.go
@@ -23,7 +23,8 @@ type TooBig struct {
 func ReadConfig(path string) (TooBig, error) {
 	cfg, err := readConfig(path)
 	if err != nil {
-		return cfg, fmt.Errorf("Failed while reading config file %s: %w", path, err)
+		// TODO: I really want to capitalize Failed here, since it is the start of an error message.
+		return cfg, fmt.Errorf("failed while reading config file %s: %w", path, err)
 	}
 	return cfg, err
 }
