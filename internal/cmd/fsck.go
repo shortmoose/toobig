683d720efb339b6e99139b62de5c46f0d23930af
Nathan Bullock <nathanbullock@gmail.com>
2025-06-20 16:44:54 -0700

    x

--- internal/cmd/fsck.go
+++ internal/cmd/fsck.go
@@ -11,73 +11,90 @@ import (
 	"github.com/shortmoose/toobig/internal/config"
 )
 
-// Basic details:
-// fsck, minimal output, display errors.
-// for each section it should output the total number of files it processed.
 func Fsck(ctx *base.Context) error {
 	fmt.Println("Performing fsck")
 
-	fmt.Println("\nValidating blobs:")
+	return fsckRestore(ctx, "Fsck", false)
+}
+
+func Restore(ctx *base.Context) error {
+	// TODO: Should we enforce that this directory is empty?
+	if !filepath.IsAbs(ctx.FilePathOverride) {
+		fmt.Fprintf(os.Stderr, "--file-path=%s isn't a full path.\n", ctx.FilePathOverride)
+		os.Exit(3)
+	}
+
+	fmt.Printf("Performing restore to %s\n", ctx.FilePathOverride)
+
+	return fsckRestore(ctx, "Restore", true)
+}
+
+func fsckRestore(ctx *base.Context, op string, restore bool) error {
 	curr, cnt, cnt_e := "/", 0, 0
-	start := time.Now()
-	waiting := true
+	var err error
+	if !restore {
+		fmt.Println("\nValidating blobs:")
+		start := time.Now()
+		waiting := true
 
-	err := base.ChdirWalk(ctx.BlobPath, func(path string, info fs.DirEntry) error {
-		if len(path) != 64 {
-			fmt.Fprintf(os.Stderr, "Blob '%s': doesn't have a valid name\n", path)
-			cnt_e += 1
-			return nil
-		}
-
-		// Display a progress bar (sort of).
-		if !ctx.Verbose {
-			if curr != path[:len(curr)] {
-				curr = path[:len(curr)]
-				// TODO: should have a bool that says if we will need a newline.
-				fmt.Printf("%s..", curr)
+		err = base.ChdirWalk(ctx.BlobPath, func(path string, info fs.DirEntry) error {
+			if len(path) != 64 {
+				fmt.Fprintf(os.Stderr, "Blob '%s': doesn't have a valid name\n", path)
+				cnt_e += 1
+				return nil
 			}
 
-			// Increase granularity if this is going to take a long time.
-			if waiting && time.Since(start).Seconds() > 100 {
-				waiting = false
-				if curr == "0" {
-					curr = "//"
+			// Display a progress bar (sort of).
+			if !ctx.Verbose {
+				if curr != path[:len(curr)] {
+					curr = path[:len(curr)]
+					// TODO: should have a bool that says if we will need a newline.
+					fmt.Printf("%s..", curr)
+				}
+
+				// Increase granularity if this is going to take a long time.
+				if waiting && time.Since(start).Seconds() > 100 {
+					waiting = false
+					if curr == "0" {
+						curr = "//"
+					}
 				}
 			}
-		}
 
-		sha, er := base.GetSha256(path)
-		if er != nil {
-			fmt.Fprintf(os.Stderr, "Blob '%s...': %v\n", path[:8], er)
-			cnt_e += 1
+			sha, er := base.GetSha256(path)
+			if er != nil {
+				fmt.Fprintf(os.Stderr, "Blob '%s...': %v\n", path[:8], er)
+				cnt_e += 1
+				return nil
+			}
+
+			if path != sha {
+				fmt.Fprintf(os.Stderr, "Blob '%s...': checksum doesn't match\n", path[:8])
+				cnt_e += 1
+				return nil
+			}
+
+			if ctx.Verbose {
+				fmt.Printf("Blob '%s...' valid.\n", path[:8])
+			}
+
+			cnt += 1
 			return nil
+		})
+		if err != nil {
+			return err
 		}
 
-		if path != sha {
-			fmt.Fprintf(os.Stderr, "Blob '%s...': checksum doesn't match\n", path[:8])
-			cnt_e += 1
-			return nil
+		if !ctx.Verbose {
+			fmt.Println("")
 		}
-
-		if ctx.Verbose {
-			fmt.Printf("Blob '%s...' valid.\n", path[:8])
+		if cnt_e != 0 {
+			fmt.Fprintf(os.Stderr, "\nFsck failed: %d blobs validated, %d errors\n", cnt, cnt_e)
+			os.Exit(11)
 		}
+		fmt.Printf("%d blobs validated, %d errors.\n", cnt, cnt_e)
 
-		cnt += 1
-		return nil
-	})
-	if err != nil {
-		return err
 	}
-
-	if !ctx.Verbose {
-		fmt.Println("")
-	}
-	if cnt_e != 0 {
-		fmt.Fprintf(os.Stderr, "\nFsck failed: %d blobs validated, %d errors\n", cnt, cnt_e)
-		os.Exit(11)
-	}
-	fmt.Printf("%d blobs validated, %d errors.\n", cnt, cnt_e)
 
 	fmt.Println("\nValidating refs:")
 	cnt, cnt_e = 0, 0
@@ -103,9 +120,28 @@ func Fsck(ctx *base.Context) error {
 			return nil
 		}
 
+		if restore {
+			files_path := filepath.Join(ctx.FilePathOverride, path)
+			d := filepath.Dir(files_path)
+			er = os.MkdirAll(d, 0700)
+			if er != nil {
+				fmt.Fprintf(os.Stderr, "mkdir '%s': %v\n", d, er)
+				cnt_e += 1
+				return nil
+			}
+
+			er = os.Link(blob_path, files_path)
+			if er != nil {
+				fmt.Fprintf(os.Stderr, "linking %s to %s: %v\n", files_path, blob_path, er)
+				cnt_e += 1
+				return nil
+			}
+		}
+
 		cnt += 1
 		if ctx.Verbose {
 			fmt.Printf("Ref '%s': valid.\n", path)
+			fmt.Printf("Ref '%s': restored.\n", path)
 		}
 		return nil
 	})
@@ -114,11 +150,11 @@ func Fsck(ctx *base.Context) error {
 	}
 
 	if cnt_e != 0 {
-		fmt.Fprintf(os.Stderr, "\nFsck failed: %d refs validated, %d errors\n", cnt, cnt_e)
+		fmt.Fprintf(os.Stderr, "\n%s failed: %d refs validated, %d errors\n", op, cnt, cnt_e)
 		os.Exit(11)
 	}
 	fmt.Printf("%d refs validated, %d errors.\n", cnt, cnt_e)
 
-	fmt.Println("\nfsck complete.")
+	fmt.Printf("\n%s complete.\n", op)
 	return nil
 }
--- internal/cmd/status.go
+++ internal/cmd/status.go
@@ -4,26 +4,50 @@ import (
 	"fmt"
 	"io/fs"
 	"os"
+	"strings"
 
 	"github.com/shortmoose/toobig/internal/base"
 )
 
 func Status(ctx *base.Context) error {
-	fmt.Println("Performing status")
+	return statusUpdate(ctx, "Status", false)
+}
 
-	// ########
-	fmt.Println("\nValidating files:")
+func Update(ctx *base.Context) error {
+	return statusUpdate(ctx, "Update", true)
+}
+
+func statusUpdate(ctx *base.Context, op string, update bool) error {
+	fmt.Printf("Performing %s\n", strings.ToLower(op))
+
+	blob_index := make(map[string]bool)
+
+	fmt.Println("\nExamining files:")
 	cnt, cnt_u, cnt_e := 0, 0, 0
 	err := base.ChdirWalk(ctx.FilePath, func(path string, info fs.DirEntry) error {
 		cnt += 1
-		_, ix, er := verifyMeta(ctx, path)
+		ref, ix, er := verifyMeta(ctx, path)
 		if er != nil {
 			cnt_e += 1
 			fmt.Fprintf(os.Stderr, "File '%s': %v\n", path, er)
 			return nil
 		}
 		if ix == nil {
+			blob_index[ref] = true
 			return nil
+		}
+
+		if update {
+			// TODO: Should the old Ref be moved to old??
+			// See normal/file_updated test
+			// TODO: See update-dup-and-linked, link created multiple times?
+			ref, er = updateMeta(ctx, path)
+			if er != nil {
+				cnt_e += 1
+				fmt.Fprintf(os.Stderr, "File '%s': %v\n", path, er)
+				return nil
+			}
+			blob_index[ref] = true
 		}
 
 		cnt_u += 1
@@ -35,7 +59,7 @@ func Status(ctx *base.Context) error {
 	}
 
 	if cnt_e != 0 {
-		fmt.Fprintf(os.Stderr, "Status failed: %d files, %d updated, %d errors\n", cnt, cnt_u, cnt_e)
+		fmt.Fprintf(os.Stderr, "%s failed: %d files, %d updated, %d errors\n", op, cnt, cnt_u, cnt_e)
 		os.Exit(11)
 	}
 	u := (cnt_u > 0)
@@ -55,7 +79,18 @@ func Status(ctx *base.Context) error {
 			return nil
 		}
 
+		if update {
+			er = mvToOld(ctx, path, "refs")
+			if er != nil {
+				cnt_e += 1
+				fmt.Fprintf(os.Stderr, "Ref %s: %v\n", path, er)
+				return nil
+			}
+
+		}
+
 		cnt_u += 1
+		fmt.Printf("Ref '%s': moved to old.\n", path)
 		fmt.Printf("Ref '%s': to be deleted\n", path)
 		return nil
 	})
@@ -64,12 +99,48 @@ func Status(ctx *base.Context) error {
 	}
 
 	if cnt_e != 0 {
-		fmt.Fprintf(os.Stderr, "Status failed: %d refs validated, %d errors\n", cnt, cnt_e)
+		fmt.Fprintf(os.Stderr, "%s failed: %d refs validated, %d errors\n", op, cnt, cnt_e)
 		os.Exit(11)
 	}
 	fmt.Printf("%d refs validated, %d errors.\n", cnt, cnt_e)
+	u = (u || cnt_u > 0)
 
-	fmt.Println("\nStatus complete.")
+	if update {
+		fmt.Printf("\nCleaning up blobs:\n")
+		cnt_u, cnt_e = 0, 0
+		err = base.ChdirWalk(ctx.BlobPath, func(path string, info fs.DirEntry) error {
+			if blob_index[path] {
+				return nil
+			}
+
+			name := path
+			if len(name) == 64 {
+				name = name[:8]
+			}
+
+			er := mvToOld(ctx, path, "blobs")
+			if er != nil {
+				fmt.Fprintf(os.Stderr, "Blob '%s': %v\n", name, er)
+				cnt_e += 1
+				return nil
+			}
+			cnt_u += 1
+			fmt.Printf("Blob '%s': moved to old.\n", name)
+			return nil
+		})
+		if err != nil {
+			return err
+		}
+
+		if cnt_e != 0 {
+			fmt.Fprintf(os.Stderr, "Clean up failed: %d moved, %d errors\n", cnt_u, cnt_e)
+			os.Exit(11)
+		}
+		fmt.Printf("%d moved.\n", cnt_u)
+		u = (u || cnt_u > 0)
+	}
+
+	fmt.Printf("\n%s complete.\n", op)
 	if ctx.UpdateIsError && (cnt_u > 0 || u) {
 		os.Exit(10)
 	}
